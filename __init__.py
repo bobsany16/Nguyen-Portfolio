d38e5e839517f1d82909371ee0c97975fa7fae0c
Bobby Nguyen <nguyenanhlinh1998@yahoo.com>
2019-07-23 15:34:24 -0400

    Initial Commit

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path="/static")
+
+@app.route("/")
+def root(): 
+    return render_template("nguyen_layout.html")
+
+if __name__ == "__main__": 
+    app.run(debug=True)
+
